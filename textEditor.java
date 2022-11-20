d9c3efe70a559becc7c9c04232edbaa61d2fdb0b
ConnerMurowchick <104602083+ConnerMurowchick@users.noreply.github.com>
2022-11-19 21:15:01 -0800

    Creates a window frame on run

--- textEditor.java
+++ textEditor.java
@@ -1,5 +1,15 @@
+import java.awt.*; 
+import javax.swing.*; 
 public class textEditor {
     public static main(String args[]) {
-        
+        createWindow();
     }
+    private static void createWindow() {
+        JFrame frame = new JFrame();
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Text Editor");
+        frame.setVisible(true);
+    }
+
 }
