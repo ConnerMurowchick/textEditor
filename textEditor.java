44d9e44e1186c4ddd66eb63439b7142c4504e8fb
ConnerMurowchick <104602083+ConnerMurowchick@users.noreply.github.com>
2022-11-21 21:56:43 -0800

    begun work on menu bars

--- textEditor.java
+++ textEditor.java
@@ -1,15 +1,61 @@
 import java.awt.*; 
 import javax.swing.*; 
+import java.awt.event.*;
 public class textEditor {
-    public static main(String args[]) {
+    public static void main(String args[]) {
         createWindow();
     }
     private static void createWindow() {
-        JFrame frame = new JFrame();
+        // make frame 
+        JFrame frame = new JFrame();    
         frame.setSize(400,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Text Editor");
         frame.setVisible(true);
+        // create menu bar
+        final JMenuBar menuBar = new JMenuBar();
+        // create menus
+        JMenu fileMenu = new JMenu("File");
+        JMenu editMenu = new JMenu("Edit");
+        JMenu formatMenu = new JMenu("Format");
+        // create menu items
+        JMenuItem newMenuItem = new JMenuItem("New");
+        //newMenuItem.setMnemonic(KeyEvent.VK_N);
+        newMenuItem.setActionCommand("New");
+
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        openMenuItem.setActionCommand("Open");
+
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.setActionCommand("Save");
+
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.setActionCommand("Exit");
+
+        JMenuItem cutMenuItem = new JMenuItem("Cut");
+        cutMenuItem.setActionCommand("Cut");
+
+        JMenuItem copyMenuItem = new JMenuItem("Copy");
+        copyMenuItem.setActionCommand("Copy");
+
+        JMenuItem pasteMenuItem = new JMenuItem("Paste");
+        pasteMenuItem.setActionCommand("Paste");
+
+        MenuItemListener menuItemListener = new MenuItemListener();
+
+        newMenuItem.addActionListener(menuItemListener);
+        openMenuItem.addActionListener(menuItemListener);
+        saveMenuItem.addActionListener(menuItemListener);
+        exitMenuItem.addActionListener(menuItemListener);
+        cutMenuItem.addActionListener(menuItemListener);
+        copyMenuItem.addActionListener(menuItemListener);
+        pasteMenuItem.addActionListener(menuItemListener);
+
     }
+    class MenuItemListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {            
+           statusLabel.setText(e.getActionCommand() + " JMenuItem clicked.");
+        }    
+     }
 
 }
