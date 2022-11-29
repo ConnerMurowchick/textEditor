832cf4674ccf50c35b240d9fa1f130bcba07acec
ConnerMurowchick <104602083+ConnerMurowchick@users.noreply.github.com>
2022-11-28 18:57:35 -0800

    Begun work on "save" methods

--- textEditor.java
+++ textEditor.java
@@ -1,6 +1,8 @@
 import java.awt.*; 
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.*;
+import java.io.*;
 /********** TO DO ***************
  * Refactor code into more methods for better readability
  * Add a margin to the left and top of the text area
@@ -8,6 +10,10 @@ import java.awt.event.*;
  * Add functionality to all menu buttons
  * Make program exit on close
  */
+
+ /*
+  * NEXT: Make clicking the save button display something with actionlistener
+  */
 public class textEditor implements ActionListener {
      // menubar
     static JMenuBar mb;
@@ -103,5 +109,47 @@ public class textEditor implements ActionListener {
         // set the label to the menuItem that is selected
         l.setText(s + " selected");
     }
+    private void SaveActionPerformed(java.awt.event.ActionEvent evt) {
+        String name = "New File";
+        ArrayList<String> Text = new ArrayList<String>();
     
+        Text.add(jt.getText());
+    
+        SaveFile(name, Text);
+    } 
+    public void SaveFile(String name, ArrayList< String> message) {
+
+        String path = "C:\\Users\\Conner\\Desktop\\" + name + ".txt";
+        File file1 = new File(path);
+        try {
+            if (!file1.exists()) {
+                file1.createNewFile();
+            }
+            File[] files = file1.listFiles();
+            FileWriter fw = new FileWriter(file1, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (int i = 0; i < message.size(); i++) {
+                bw.write(message.get(i));
+                bw.newLine();
+            }
+            bw.close();
+            fw.close();
+            FileReader fr = new FileReader(file1);
+            BufferedReader br = new BufferedReader(fr);
+            fw = new FileWriter(file1, true);
+            bw = new BufferedWriter(fw);
+            while (br.ready()) {
+                String line = br.readLine();
+                System.out.println(line);
+                bw.write(line);
+                bw.newLine();
+            }
+            br.close();
+            fr.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error in" + ex);        
+    }
+    
+}
 }
